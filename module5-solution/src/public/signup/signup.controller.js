be8593d2222a917a3cbff6e9364701182f89ae2b
Ignacio Muñoz <ign.munoz.v@gmail.com>
2017-04-05 18:10:02 -0300

    Added Bonus Task 4 & 5

--- module5-solution/src/public/signup/signup.controller.js
+++ module5-solution/src/public/signup/signup.controller.js
@@ -6,7 +6,6 @@ angular.module('public')
 
 SignupController.$inject = ['MenuService', 'UserInfoService'];
 function SignupController(MenuService, UserInfoService){
-
 	var $ctrl = this;
 	$ctrl.error;
 	$ctrl.success;
